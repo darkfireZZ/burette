5fe4b92c58562221942106745abbbb77b6adec42
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2025-01-29 11:00:34 +0100

    Add more tests

---
+++ system_tests/tests/general/TEST_help/cmds.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+burette -h
+burette --help
+burette help
---
+++ system_tests/tests/general/invalid_library/TEST_no_index_file/cmds.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+TMPDIR=$(mktemp -d)
+# We enter the temporary directory to ensure that its path does not appear in
+# the output of the tests.
+cd $TMPDIR
+
+LIBRARY=./library
+
+burette --library $LIBRARY new
+rm $LIBRARY/index.json
+
+! burette --library $LIBRARY add $TEST_DOCS/var_chrom.pdf
+
+rm -rf $LIBRARY
+rm -d $TMPDIR
