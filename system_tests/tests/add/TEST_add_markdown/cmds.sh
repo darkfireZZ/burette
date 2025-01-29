5f73a45a7a221bb7bcbf0c05eadff28061485b6b
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2025-01-29 08:20:11 +0100

    Update markdown test

--- system_tests/tests/add/TEST_add_markdown/cmds.sh
+++ system_tests/tests/add/TEST_add_markdown/cmds.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-burette add $TEST_DOCS/markdown_file.md
+burette add $TEST_DOCS/markdown_file.md << EOF
+This is a markdown file
+n
+n
+n
+EOF
+
 if [ $? -eq 0 ]; then
     exit 1
 else
