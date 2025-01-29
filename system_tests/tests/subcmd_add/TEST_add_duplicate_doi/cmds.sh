b985506356d341ef6e4720d2ca570012d8896dd5
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2025-01-29 14:17:15 +0100

    Add test for duplicate DOIs

---
+++ system_tests/tests/subcmd_add/TEST_add_duplicate_doi/cmds.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+burette list
+
+burette add $TEST_DOCS/darwin.epub << EOF
+On the Origin of Species By Means of Natural Selection
+yeS
+Charles Darwin
+N
+no
+y
+10.5962/bhl.title.59991
+EOF
+echo
+burette list
+
+! burette add $TEST_DOCS/moby_dick_2.epub << EOF
+On the Origin of Species By Means of Natural Selection
+Yes
+Charles Darwin
+no
+No
+Y
+10.5962/bhl.title.59991
+EOF
+echo
+burette list
