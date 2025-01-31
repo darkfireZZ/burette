7659001473d5af4126a73c4b927321e58a636ae0
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2025-01-31 10:24:50 +0100

    Move test from subcmd_new to file_hierarchy

--- system_tests/tests/subcmd_new/TEST_new/cmds.sh
+++ system_tests/tests/subcmd_new/TEST_new/cmds.sh
@@ -2,12 +2,5 @@
 
 set -e
 
-burette --library new_lib new
-ls new_lib
-
-echo
-echo 'index.json:'
-cat new_lib/index.json
-echo
-echo 'burette_version:'
-cat new_lib/burette_version
+burette --library=library new
+ls library
