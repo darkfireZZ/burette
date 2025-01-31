ffb4aa1f74f92456054ed08fce7bd82bc1fd6c7c
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2025-01-31 10:19:57 +0100

    Fix broken test

--- system_tests/tests/subcmd_remove/TEST_multiple_partially_ambiguous_subprefixes/cmds.sh
+++ system_tests/tests/subcmd_remove/TEST_multiple_partially_ambiguous_subprefixes/cmds.sh
@@ -18,6 +18,6 @@ echo
 burette -l $LIBRARY_PATH list && echo
 
 # $HASH_VAR_CHROM starts with 2576
-! burette -l $LIBRARY_PATH remove '' 2 25 257 2576 $HASH_VAR_CHROM $HASH_DARWIN
+! burette -l $LIBRARY_PATH remove 2 2 2 25 25 257 2576 $HASH_VAR_CHROM $HASH_DARWIN
 echo
 burette -l $LIBRARY_PATH list
